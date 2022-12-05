6d4dce3b0f6fb0c4ede384af016c1faa644c4e10
Cassian Kahema <kahemacassian@gmail.com>
2022-12-06 01:43:49 +1030

    Grid size change with size-slider - added tool icons - added more features and events

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// todo WORRY ABOUT UNITs OF SIZE px, vw, vh
 
 const canvas = document.querySelector(".canvas");
 let canvas_style = getComputedStyle(canvas);
-let grid_size = 16;
-let mouse_hold = false;
+let default_grid_size = 16;
+let drawing = false;
+let fill = true;
+export let grid_lines = true;
+
 
 // creates and adds squares to a multi-dimensional array and a canvas
-function createSquares() {
-  let squares = new Array(grid_size);
+function createSquares(size) {
+  // delete grid_map;
+  let squares = new Array(size);
   for (let i = 0; i < squares.length; i++) {
     squares[i] = new Array(squares.length);
   }
 
-  // calculate the size of the squares
+  let sq_size = `${(1 / size) * 100}%`;
 
-  let size = getCanvasSize() / grid_size;
-
-  console.log(size + " ");
+  console.log(typeof(sq_size));
   for (let i = 0; i < squares.length; i++) {
     for (let j = 0; j < squares[i].length; j++) {
-      squares[i][j] = createPixel(`${size}px`);
+      squares[i][j] = createPixel(sq_size);
     }
   }
   return squares;
@@ -32,22 +33,26 @@ function createPixel(size) {
   square.style.width = size;
   square.style.height = size;
   square.classList.toggle("pixel");
-  square.classList.toggle("grid-lines");
+  if (!grid_lines) square.classList.toggle("remove-grid-lines");
 
   square.addEventListener("mousedown", (e) => {
-    mouse_hold = true;
+    drawing = true;
     changeColor(e.currentTarget);
     console.log("Mouse down");
   });
 
   square.addEventListener("mouseup", (e) => {
-    mouse_hold = false;
+    drawing = false;
     console.log("Mouse up");
   });
 
   square.addEventListener("mouseover", (e) => {
-    if (mouse_hold) changeColor(e.currentTarget);
+    if (drawing) changeColor(e.currentTarget);
     console.log("Mouse over");
+  });
+
+  square.addEventListener("click", (e) => {
+    if (fill) mapFill(e.currentTarget);
   });
 
   canvas.appendChild(square);
@@ -55,30 +60,76 @@ function createPixel(size) {
   return square;
 }
 
+// Load the squares to the canvas
+let grid_map = createSquares(default_grid_size);
+// removeAllSquares()
+// updatePixelSize(19)
+
+
+function updatePixelSize(size) {
+  console.log(typeof(size));
+  //remove all current squares
+  removeAllSquares()
+
+  grid_map = createSquares(size);
+}
+
+function removeAllSquares(){
+
+  grid_map.forEach(row => {
+    row.forEach(sq=>{
+      sq.remove();
+    })
+  });
+  grid_map = null
+}
+
+
+let slider = document.getElementById('size-slider')
+let value_display = document.querySelector('.size.settings span')
+slider.setAttribute('min', '1')
+slider.setAttribute('max', '100')
+slider.value = default_grid_size
+let current_grid_size = default_grid_size
+
+let dragging = false;
+slider.addEventListener('input', (e)=>{
+  const value = e.currentTarget.value
+  value_display.textContent = `${value}x${value}`
+})
+slider.ondragend = () => console.log("what")
+slider.onmousedown = () => {
+  console.log("started Dragging")
+  dragging = true; // !not necessary
+}
+
+slider.onmouseup = (e) => {
+  console.log("finished draggging...")
+  if (dragging){ // ! checking if dragging not necessary
+    const value = e.currentTarget.value
+    if (value != current_grid_size) updatePixelSize(parseInt(value)) // ! Why doesn't work without parse int when its still a string
+  }
+}
+
+/* CHANGE COLOR */
 function changeColor(square) {
   square.style.background = "blue";
 }
 
-function getCanvasSize() {
-  let w = canvas_style.width;
-  w = w.slice(0, w.length - 2);
-  // include border ofsset
-  const offset = grid_size * 2;
-
-  return parseFloat(w) - offset;
+/* FILL FUNCTION */
+function mapFill(square){
+  // BFS
 }
 
+/* TOGGLE GRID-LINES */
 const toggle_grid_btn = document.getElementById("toggle-grid");
 toggle_grid_btn.addEventListener("click", function () {
   grid_map.forEach((row) => {
     row.forEach((sq) => {
-      sq.classList.toggle("grid-lines");
+      sq.classList.toggle("remove-grid-lines");
     });
   });
-
-  {
-  }
+  grid_lines = (grid_lines) ? false:true
 });
 
-// Function call on load
-let grid_map = createSquares();
+export { grid_map, updatePixelSize };

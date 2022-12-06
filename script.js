dafa93a317e9e9b830b413fc8ec5d7e58ef2d5bd
Cassian Kahema <kahemacassian@gmail.com>
2022-12-06 16:45:33 +1030

    Colour Features

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
 const canvas = document.querySelector(".canvas");
+// prevent canvas from being dragged
+canvas.setAttribute("draggable", false);
 let canvas_style = getComputedStyle(canvas);
 let default_grid_size = 16;
 let drawing = false;
 let fill = true;
 let grid_lines = true;
-
+let hasChanges = false;
 
 // creates and adds squares to a multi-dimensional array and a canvas
 function createSquares(size) {
@@ -17,7 +18,6 @@ function createSquares(size) {
 
   let sq_size = `${(1 / size) * 100}%`;
 
-  console.log(typeof(sq_size));
   for (let i = 0; i < squares.length; i++) {
     for (let j = 0; j < squares[i].length; j++) {
       squares[i][j] = createPixel(sq_size);
@@ -33,22 +33,21 @@ function createPixel(size) {
   square.style.width = size;
   square.style.height = size;
   square.classList.toggle("pixel");
+  square.style.backgroundColor = 'tranparent';
   if (!grid_lines) square.classList.toggle("remove-grid-lines");
+  square.setAttribute("draggable", false);
 
   square.addEventListener("mousedown", (e) => {
     drawing = true;
     changeColor(e.currentTarget);
-    console.log("Mouse down");
   });
 
   square.addEventListener("mouseup", (e) => {
     drawing = false;
-    console.log("Mouse up");
   });
 
   square.addEventListener("mouseover", (e) => {
     if (drawing) changeColor(e.currentTarget);
-    console.log("Mouse over");
   });
 
   square.addEventListener("click", (e) => {
@@ -65,59 +64,99 @@ let grid_map = createSquares(default_grid_size);
 // removeAllSquares()
 // updatePixelSize(19)
 
-
 function updatePixelSize(size) {
-  console.log(typeof(size));
   //remove all current squares
-  removeAllSquares()
+  removeAllSquares();
 
   grid_map = createSquares(size);
 }
 
-function removeAllSquares(){
-
-  grid_map.forEach(row => {
-    row.forEach(sq=>{
+function removeAllSquares() {
+  grid_map.forEach((row) => {
+    row.forEach((sq) => {
       sq.remove();
-    })
+    });
   });
-  grid_map = null
+  grid_map = null;
+  hasChanges = false;
 }
-
-
-let slider = document.getElementById('size-slider')
-let value_display = document.querySelector('.size.settings span')
-slider.setAttribute('min', '1')
-slider.setAttribute('max', '100')
-slider.value = default_grid_size
-let current_grid_size = default_grid_size
+/* SLIDER SECTION */
+let slider = document.getElementById("size-slider");
+let value_display = document.querySelector(".size.settings span");
+slider.setAttribute("min", "1");
+slider.setAttribute("max", "100");
+slider.value = default_grid_size;
+let current_grid_size = default_grid_size;
 
 let dragging = false;
-slider.addEventListener('input', (e)=>{
-  const value = e.currentTarget.value
-  value_display.textContent = `${value}x${value}`
-})
-slider.ondragend = () => console.log("what")
+slider.addEventListener("input", (e) => {
+  const value = e.currentTarget.value;
+  value_display.textContent = `${value}x${value}`;
+});
 slider.onmousedown = () => {
-  console.log("started Dragging")
   dragging = true; // !not necessary
-}
+};
 
 slider.onmouseup = (e) => {
-  console.log("finished draggging...")
-  if (dragging){ // ! checking if dragging not necessary
-    const value = e.currentTarget.value
-    if (value != current_grid_size) updatePixelSize(parseInt(value)) // ! Why doesn't work without parse int when its still a string
+  if (dragging) {
+    // ! checking if dragging not necessary
+    const value = e.currentTarget.value;
+    if (value != current_grid_size) updatePixelSize(parseInt(value)); // ! Why doesn't work without parse int when its still a string
   }
-}
+};
 
 /* CHANGE COLOR */
+let pencil_color = null;
+let bg_color = '#ffffff';  // default white
+let rainbow = false;
+
+// Pencil
+let pencil_color_selector = document.getElementById("pencil-color-selector");
+// default color #000000
+pencil_color = pencil_color_selector.value;
+
+// allow user to set custom colour from picker
+pencil_color_selector.addEventListener("input", () => {
+  pencil_color = pencil_color_selector.value;
+});
+
+// Background
+const bg_color_selector = document.getElementById('bg-color-selector')
+bg_color_selector.value = bg_color
+bg_color_selector.addEventListener('input', ()=>{
+  changeBgColor(bg_color_selector.value)
+})
+
+// Rainbow
+const rainbow_selector = document.querySelector('.icon.rainbow')
+rainbow_selector.addEventListener('click', (e)=>{
+  e.currentTarget.classList.toggle('selected')
+  rainbow = (rainbow)?false:true
+  console.log('rainbow: ' + `${(rainbow)?"on":"off"}`);
+})
+
 function changeColor(square) {
-  square.style.background = "blue";
+  hasChanges = true;
+  if(rainbow){
+    let r_int = (max) => Math.floor(Math.random() * (max+1))
+    let r_float = () => {
+      let rf = Math.random()
+      if (rf < 0.5) rf+=0.5
+      return rf.toFixed(3)
+    }
+
+    let random_color = 
+      `rgba(${r_int(255)}, ${r_int(255)}, ${r_int(255)}, ${r_float()})`
+    square.style.background = random_color
+  }else square.style.background = pencil_color;
+}
+
+function changeBgColor(color){
+  canvas.style.backgroundColor = color;
 }
 
 /* FILL FUNCTION */
-function mapFill(square){
+function mapFill(square) {
   // BFS
 }
 
@@ -129,5 +168,24 @@ toggle_grid_btn.addEventListener("click", function () {
       sq.classList.toggle("remove-grid-lines");
     });
   });
-  grid_lines = (grid_lines) ? false:true
+  grid_lines = grid_lines ? false : true;
 });
+
+/* CLEAR SCREEN */
+const clr_btn = document.querySelector(".icon.clear");
+clr_btn.addEventListener("click", clear);
+function clear() {
+  // can be optimized to only clear if there are any changes
+  if (hasChanges && grid_map) {
+    console.log("clear");
+    grid_map.forEach((row) => {
+      if (row) {
+        row.forEach((sq) => {
+          sq.style.backgroundColor = "transparent";
+        });
+      }
+    });
+  }
+
+  hasChanges = false;
+}
